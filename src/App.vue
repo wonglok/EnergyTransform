550aec1d88d5af6e5396ce3c09f286263906cc8f
Wong Lok <yellowhappy831@gmail.com>
2019-04-16 09:58:27 +0800

    github backup

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,23 @@
       <router-link to="/about">About</router-link>
     </div> -->
     <router-view class="full" />
-    <router-link tag="div" to="/" style="cursor: pointer; display: block; position: fixed; top: 50px; right: 20px; color: white; text-shadow: 0px 0px 4px black;">Home</router-link>
-    <router-link tag="div" to="/routes" style="cursor: pointer; display: block; position: fixed; top: 20px; right: 20px; color: white; text-shadow: 0px 0px 4px black;">Routes</router-link>
+    <router-link tag="div" to="/" class="ya-home">Home</router-link>
+    <router-link tag="div" to="/routes" class="ya-routes">Routes</router-link>
+    <div  class="ya-fullscreen" @click="goFullscreen">FullScreen</div>
   </div>
 </template>
+<script>
+import screenfull from 'screenfull'
+export default {
+  methods: {
+    goFullscreen () {
+      if (screenfull.enabled) {
+        screenfull.request();
+      }
+    }
+  }
+}
+</script>
 
 <style>
 html, body{
@@ -23,5 +36,34 @@ html, body{
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+.ya-routes{
+  cursor: pointer;
+  display: block;
+  position: fixed;
+  top: 50px;
+  right: 20px;
+  color: white;
+  text-shadow: 0px 0px 4px black;
+}
+.ya-home{
+  cursor: pointer;
+  display: block;
+  position: fixed;
+  right: 20px;
+  top: 20px;
+  color: white;
+  text-shadow: 0px 0px 4px black;
+}
+
+.ya-fullscreen{
+  cursor: pointer;
+  display: block;
+  position: fixed;
+  right: 20px;
+  top: 80px;
+  color: white;
+  text-shadow: 0px 0px 4px black;
 }
 </style>
